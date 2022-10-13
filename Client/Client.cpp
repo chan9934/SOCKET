2c0f1a54b363915fa8426627c6b602e093c19c16
chan9934 <106580671+chan9934@users.noreply.github.com>
2022-10-14 01:51:52 +0900

    [김동찬] SOCKET Socket함수의 이해

---
+++ Client/Client.cpp
@@ -0,0 +1,76 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+#include <string>
+
+
+using std::string;
+
+void ErrorHandling(string message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char message[30];
+	int strLen = 0;
+	int Idx = 0;
+	int readLen = 0;
+	if (argc != 3)
+	{
+		printf("Usage : %s <IP><port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		ErrorHandling("WSAStartup() error!");
+	}
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+	if (hSocket == INVALID_SOCKET)
+	{
+		ErrorHandling("socket() error");
+	}
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAddr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+	{
+		ErrorHandling("connect() error!");
+	}
+
+	while (readLen = recv(hSocket, &message[++Idx], 1, 0))
+	{
+		if (readLen == -1)
+		{
+			ErrorHandling("read() error!");
+		}
+		
+			strLen += readLen;
+
+	}
+
+	printf("Message from server: %s \n", message);
+	printf("FUNCTION read call count : %d \n", strLen);
+	
+}
+
+
+
+void ErrorHandling(string message)
+{
+	char* message1 = const_cast<char*>(message.c_str());
+	fputs(message1, stderr);
+	fputc('\n', stderr);
+	exit(1);
+
+}
