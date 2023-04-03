f54f0cd21d08ea93c92bbfd9ddab919bdf3ad45a
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-04-03 20:26:20 +0900

    [기몽찬]

---
+++ Client/Mu.cpp
@@ -0,0 +1,69 @@
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS
+	#include<iostream>
+	#include<WS2tcpip.h>
+
+	#define BUF_SIZE 30
+
+	using namespace std;
+
+	void ErrorHandling(const char* message)
+	{
+		printf("%s\n", message);
+		exit(1);
+	}
+
+	int main(int argc, char* argv[])
+	{
+		WSADATA wsaData;
+		SOCKET hRecvSock;
+		SOCKADDR_IN adr;
+		struct ip_mreq joinAdr;
+		char buf[BUF_SIZE];
+		int strLen;
+
+		if (argc != 3)
+		{
+			printf("USage : %s <IP> <PORT>\n", argv[0]);
+			exit(1);
+		}
+
+		if (0 != WSAStartup(MAKEWORD(2, 2), &wsaData))
+		{
+			ErrorHandling("WSAStartup Error");
+		}
+
+		hRecvSock = socket(PF_INET, SOCK_DGRAM, 0);
+
+		memset(&adr, 0, sizeof(adr));
+		adr.sin_family = AF_INET;
+		adr.sin_addr.s_addr = htonl(INADDR_ANY);
+		adr.sin_port = htons(atoi(argv[2]));
+
+		if (bind(hRecvSock, (SOCKADDR*)&adr, sizeof(adr)) == SOCKET_ERROR)
+		{
+			ErrorHandling("bindError");
+		}
+
+		joinAdr.imr_multiaddr.s_addr = inet_addr(argv[1]);
+		joinAdr.imr_interface.s_addr = htonl(INADDR_ANY);
+		if (setsockopt(hRecvSock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*)&joinAdr, sizeof(joinAdr)) == SOCKET_ERROR)
+		{
+			ErrorHandling("setsockopt Error");
+		}
+
+		while (1)
+		{
+			strLen = recvfrom(hRecvSock, buf, sizeof(buf) - 1, 0, NULL, 0);
+			if (strLen < 0)
+			{
+				break;
+			}
+			buf[strLen] = 0;
+			printf(buf);
+		}
+		closesocket(hRecvSock);
+		WSACleanup();
+		return 0;
+
+
+	}
